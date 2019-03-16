7bb79448fef13bcfd36a0db87db7509a9253ece5
george ballard <Theballardian@outlook.com>
2019-03-16 12:24:10 +0800

    added routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/portfolio', to: 'pages#portfolio'
+  get '/about', to: 'pages#about'
+  get '/resume', to: 'pages#resume'
+  get '/contact', to: 'pages#contact'
 end
